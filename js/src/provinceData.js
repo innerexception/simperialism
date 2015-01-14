708bcd5e2d4504490da278da4a5b75dad1ebf6bd
Jonathan Baker <jbakerdev@gmail.com>
2015-01-14 11:45:45 -0700

    Map drawing done exept data entry

--- js/src/provinceData.js
+++ js/src/provinceData.js
@@ -3,10 +3,10 @@ define(['candy'], function(Candy){
        {
            name: 'Bratlslava',
            edges:[
-               {lineWidth:'4', color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [20,6,20], startX:40, startY:35, endX:120, endY:60},
-               {lineWidth:'4', color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [20,6,20], startX:120, startY:60, endX:140, endY:160},
-               {lineWidth:'2', color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:140, startY:160, endX:90, endY:200},
-               {lineWidth:'2', color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:90, startY:200, endX:40, endY:35}
+               {lineWidth:4, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [20,6,20], startX:40, startY:35, endX:120, endY:60},
+               {lineWidth:4, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [20,6,20], startX:120, startY:60, endX:140, endY:160},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:140, startY:160, endX:90, endY:200},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:90, startY:200, endX:40, endY:35}
            ]
        },
        {
@@ -28,7 +28,7 @@ define(['candy'], function(Candy){
            ]
        },
        {
-           name: '',
+           name: 'Grimrock',
            edges:[
                {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [20,6,20], startX:90, startY:200, endX:60, endY:350},
                {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [20,6,20], startX:60, startY:350, endX:160, endY:420},
@@ -36,13 +36,61 @@ define(['candy'], function(Candy){
            ]
        },
        {
-           name: '',
+           name: 'Yakterberg',
            edges:[
                {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:90, startY:200, endX:200, endY:210},
                {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:200, startY:210, endX:210, endY:330},
                {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:210, startY:330, endX:160, endY:420},
                {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:160, startY:420, endX:90, endY:200}
            ]
+       },
+       {
+           name: 'Magdengrad',
+           edges:[
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:280, startY:150, endX:420, endY:180},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:420, startY:180, endX:320, endY:210},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:320, startY:210, endX:280, endY:300},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:280, startY:300, endX:210, endY:330},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:210, startY:330, endX:200, endY:210},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:200, startY:210, endX:280, endY:150}
+           ]
+       },
+       {
+           name: 'Macondo',
+           edges:[
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:210, startY:330, endX:220, endY:490},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:220, startY:490, endX:260, endY:580},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:260, startY:580, endX:170, endY:580},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:170, startY:580, endX:90, endY:540},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:90, startY:540, endX:210, endY:330}
+           ]
+       },
+       {
+           name: 'Macondo',
+           edges:[
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:210, startY:330, endX:280, endY:300},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:280, startY:300, endX:380, endY:420},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:380, startY:420, endX:220, endY:490},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:220, startY:490, endX:210, endY:330}
+           ]
+       },
+       {
+           name: 'Macondo',
+           edges:[
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:90, startY:540, endX:170, endY:580},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:170, startY:580, endX:260, endY:580},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:260, startY:580, endX:150, endY:660},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:150, startY:660, endX:90, endY:540}
+           ]
+       },
+       {
+           name: 'Macondo',
+           edges:[
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:150, startY:660, endX:260, endY:580},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:260, startY:580, endX:310, endY:660},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:310, startY:660, endX:290, endY:690},
+               {lineWidth:2, color:Candy.gameBoyPalette.darkBlueGreen, lineDash: [6,6,2], startX:290, startY:690, endX:150, endY:660}
+           ]
        }
-   ];
+   ]
 });
--- js/src/worldmap.js
+++ js/src/worldmap.js
@@ -18,18 +18,19 @@ define(['lodash', 'provinceData', 'candy'], function(_, ProvinceData, Candy){
 
         this.phaserInstance = phaserInstance;
         this.mapBmp = this.phaserInstance.add.bitmapData(1024, 768);
-
     };
     worldMap.prototype = {
         update: function(){
             _.each(this.provinces, function(province){
                 if(province.polygon ? province.polygon.contains(this.phaserInstance.input.x, this.phaserInstance.input.y) : false){
                     //Draw fill polygon
-                    console.log('drawing fill poly' + province.polygon);
+                    console.log('drawing fill poly');
+                    province.polyDrawer.beginFill(0xFFFFFF, 0.5);
+                    province.polyDrawer.drawPolygon(province.polygon);
                 }
                 else{
                     //Clear polygon
-
+                    province.polyDrawer ? province.polyDrawer.clear() : false;
                 }
             }, this);
         },
@@ -50,24 +51,23 @@ define(['lodash', 'provinceData', 'candy'], function(_, ProvinceData, Candy){
             this.mapSprite = this.phaserInstance.add.sprite(0,0,this.mapBmp);
         },
         drawProvince: function(province){
-            this.mapBmp.ctx.beginPath();
             var polygonPoints = [];
             _.each(province.edges, function(edge){
+                this.mapBmp.ctx.beginPath();
                 this.mapBmp.ctx.lineWidth = edge.lineWidth;
                 this.mapBmp.ctx.strokeStyle = edge.color;//'rgb(239,237,79)';
                 this.mapBmp.ctx.setLineDash(edge.lineDash);//[2,3]);
                 this.mapBmp.ctx.moveTo(edge.startX,edge.startY);
                 this.mapBmp.ctx.lineTo(edge.endX, edge.endY);
                 polygonPoints.push({x:edge.endX, y:edge.endY});
+                this.mapBmp.ctx.closePath();
+                this.mapBmp.ctx.stroke();
             }, this);
             province.polygon = new Phaser.Polygon(polygonPoints);
-            this.mapBmp.ctx.closePath();
-            this.mapBmp.ctx.stroke();
+            province.polyDrawer = this.phaserInstance.add.graphics(0,0);
         },
         clearMap: function(){
-            _.each(ProvinceData, function(province){
-                if(province.sprite) province.sprite.destroy();
-            }, this);
+            this.mapSprite.destroy();
         }
     };
     return worldMap;
